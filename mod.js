4eabea16f498794e1bdc651e6807dd39ae41de25
Ivan Grishin <r@exah.me>
2021-12-07 23:36:40 +0000

    ⚡ Reduce bundle by 20 B
    And use arrow functions

--- mod.js
+++ mod.js
@@ -1,48 +1,40 @@
-let UNKNOWN = Symbol()
+let UNKNOWN = []
 
-function isObject(input) {
-  return input != null && typeof input === 'object'
-}
+const isObject = (input) => input != null && typeof input === 'object'
 
-function compare(input) {
-  return function (pattern) {
-    if (pattern === Boolean) return typeof input === 'boolean'
-    if (pattern === String) return typeof input === 'string'
-    if (pattern === Number) return typeof input === 'number'
-    if (pattern === Symbol) return typeof input === 'symbol'
-    if (pattern === BigInt) return typeof input === 'bigint'
+const compare = (input) => (pattern) => {
+  if (pattern === Boolean) return typeof input === 'boolean'
+  if (pattern === String) return typeof input === 'string'
+  if (pattern === Number) return typeof input === 'number'
+  if (pattern === Symbol) return typeof input === 'symbol'
+  if (pattern === BigInt) return typeof input === 'bigint'
 
-    if (isObject(pattern)) {
-      return (
-        isObject(input) &&
-        Object.keys(pattern).every(function (key) {
-          return compare(input[key])(pattern[key])
-        })
-      )
-    }
-
-    return Object.is(input, pattern)
+  if (isObject(pattern)) {
+    return (
+      isObject(input) &&
+      Object.keys(pattern).every((key) => compare(input[key])(pattern[key]))
+    )
   }
+
+  return Object.is(input, pattern)
 }
 
-export function match(input, output = UNKNOWN) {
-  return {
-    with(...patterns) {
-      let callback = patterns.pop()
-      if (patterns.some(compare(input))) output = callback(input)
-      return this
-    },
-    exhaustive(message) {
-      if (output === UNKNOWN) throw new Error(message)
-      return output
-    },
-    otherwise(cb) {
-      if (output === UNKNOWN) return cb(input)
-      return output
-    },
-    run() {
-      if (output === UNKNOWN) return
-      return output
-    },
-  }
-}
+export const match = (input, output = UNKNOWN) => ({
+  with(...patterns) {
+    let callback = patterns.pop()
+    if (patterns.some(compare(input))) output = callback(input)
+    return this
+  },
+  exhaustive(message) {
+    if (output === UNKNOWN) throw new Error(message)
+    return output
+  },
+  otherwise(cb) {
+    if (output === UNKNOWN) return cb(input)
+    return output
+  },
+  run() {
+    if (output === UNKNOWN) return
+    return output
+  },
+})
